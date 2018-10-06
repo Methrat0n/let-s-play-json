481e6ee577df23ef4fcbd7b9ee8a903caf1333f7
merlin goulet <mgo@zengularity.com>
2018-10-06 12:42:52 +0200

    implement the isJsonTest

--- src/letsplayjson/json/JsonParser.java
+++ src/letsplayjson/json/JsonParser.java
@@ -1,17 +1,58 @@
 package letsplayjson.json;
 
+import java.util.stream.Stream;
+
 public class JsonParser {
   public static boolean isJson(String maybeJson) {
-    throw new UnsupportedOperationException("not yet");
+    return isJsonObject(maybeJson) || isJsonArray(maybeJson);
   }
 
+  /**
+   * Test if a given string is a valid json object
+   * for one level. It does this by validating the bracket around the string
+   * and checking if the string compose of "key": "value" whatever the value may be
+   */
   private static boolean isJsonObject(String maybeJsonObject) {
-    throw new UnsupportedOperationException("not yet");
+    final int length = maybeJsonObject.length();
+
+    if(length < 2) //minimum json is "{}"
+      return false;
+
+    final char firstCharacter = maybeJsonObject.charAt(0);
+    final char lastCharacter = maybeJsonObject.charAt(length - 1);
+
+    final boolean asBrackets = firstCharacter == '{' && lastCharacter == '}';
+    if(length == 2 && asBrackets)
+      return true;
+
+    final String inBrackets = maybeJsonObject.substring(1,length - 2);
+    return Stream.of(inBrackets.split(","))
+        .allMatch(inBracket -> inBracket.split(":").length == 2);
   }
 
+  /**
+   * Test if a given string is a valid json array
+   * for one level. It does this by validating the [] around it and
+   * checking if the subelement are made of [] or {} separated by commas
+   */
   private static boolean isJsonArray(String maybeJsonArray) {
-    throw new UnsupportedOperationException("not yet");
+    final int length = maybeJsonArray.length();
+
+    if(length < 2) //minimum json is "[]"
+      return false;
+
+    final char firstCharacter = maybeJsonArray.charAt(0);
+    final char lastCharacter = maybeJsonArray.charAt(length - 1);
+
+    final boolean asBrackets = firstCharacter == '[' && lastCharacter == ']';
+    if(length == 2 && asBrackets)
+      return true;
+
+    final String inBrackets = maybeJsonArray.substring(1,length - 2);
+    return Stream.of(inBrackets.split(","))
+        .allMatch(inBracket -> inBracket.matches("^(\\[.*\\])|(\\{.*\\})$"));
   }
+
 
   public static String transcodeJson(String json) throws NotJsonException {
     throw new UnsupportedOperationException("not yet");
